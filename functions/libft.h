7b2f9da3b8a4398606ddfeca9a6b1c0e6faebb72
Arnalove <chansarelarno@gmail.com>
2023-01-13 17:42:18 +0100

    clean libft.h

--- functions/libft.h
+++ functions/libft.h
@@ -6,7 +6,7 @@
 /*   By: achansar <achansar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/06 09:23:52 by achansar          #+#    #+#             */
-/*   Updated: 2023/01/13 16:24:58 by achansar         ###   ########.fr       */
+/*   Updated: 2023/01/13 17:41:42 by achansar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,8 +26,6 @@ typedef struct s_list
 int		ft_printf(const char *str, ...);
 int		ft_isdigit(char *str);
 size_t	ft_atoi(const char *str);
-void	*ft_calloc(size_t count, size_t size);
-char	*ft_itoa(int n);
 t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *new);
 int		ft_lstsize(t_list *lst);
@@ -35,11 +33,8 @@ t_list	*ft_lstlast(t_list *lst, int stop);
 void	ft_lstadd_back(t_list **lst, t_list *new);
 void	ft_lstclear(t_list **lst);
 char	**ft_split(char const *s, char c);
-char	*ft_strchr(const char *s, int c);
-char	*ft_strjoin(char const *s1, char const *s2);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlen(const char *s);
-char	*ft_strrchr(const char *s, int c);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 
 #endif
